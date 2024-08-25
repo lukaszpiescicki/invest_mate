9fe95fd3bd430798db0212b254a10b5bf1fedb63
Lukasz <lukasz.piescicki@gmail.com>
2024-08-25 10:44:53 +0200

    refactor: format files

--- stock_market/apps.py
+++ stock_market/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class StockMarketConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stock_market'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "stock_market"
--- workbooks/apps.py
+++ workbooks/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class WorkbooksConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'workbooks'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "workbooks"
