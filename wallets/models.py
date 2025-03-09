345a0044bd991f1bb47b5cc3a159c4471b808ca5
Lukasz <lukasz.piescicki@gmail.com>
2025-03-09 10:33:17 +0100

    feat: add wallets

--- wallets/models.py
+++ wallets/models.py
@@ -1,1 +1,9 @@
-# Create your models here.
+from django.db import models
+
+
+class Wallet(models.Model):
+    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    stripe_payment_intent = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f"Wallet Balance: {self.balance}"
