9db4e4d83640e25df0c829b66c1751413ac8c61a
Lukasz <lukasz.piescicki@gmail.com>
2025-05-13 20:51:15 +0200

    refactor: admin panel StockAdmin view adjusted

--- stocks/admin.py
+++ stocks/admin.py
@@ -2,7 +2,13 @@ from django.contrib import admin
 
 from .models import Stock, StockOperation, StockPrice, UserStock
 
-admin.site.register(Stock)
 admin.site.register(UserStock)
 admin.site.register(StockOperation)
 admin.site.register(StockPrice)
+
+
+class StockAdmin(admin.ModelAdmin):
+    list_display = ("id", "ticker", "name")
+
+
+admin.site.register(Stock, StockAdmin)
